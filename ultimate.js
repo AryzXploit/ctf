3775c18a8082140e6b95df6b2ed915c30e9b3ee8
Muhammad Arya Arjuna Habibulah <arjunaajalahla100@gmail.com>
2025-11-16 16:46:20 +0700

    Create ultimate.js

---
+++ ultimate.js
@@ -0,0 +1,137 @@
+const _0x1a2b='v3.0';const _0x3c4d=(function(){const _0x5e6f=['\x41\x52\x59\x5a\x5a','\x58\x50\x4c\x4f\x49','\x54\x7b','\x7d'];return{_0x7g8h:_0x5e6f[0],_0x9i0j:_0x5e6f[1],_0xk1l2:_0x5e6f[2],_0xm3n4:_0x5e6f[3]}})();window._0xo5p6=_0x3c4d;window._0xq7r8={_0xs9t0:null,_0xu1v2:null,_0xw3x4:null,_0xy5z6:null,_0xa7b8:null,_0xc9d0:null,_0xe1f2:null,_0xg3h4:null,_0xi5j6:null,_0xk7l8:null};window._0xm9n0=[];window._0xo1p2=[];window._0xq3r4=[];
+
+(function(){let _0xs5t6=0;document['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x63\x6c\x69\x63\x6b',function(_0xu7v8){_0xs5t6++;window._0xm9n0['\x70\x75\x73\x68']({x:_0xu7v8['\x63\x6c\x69\x65\x6e\x74\x58']%100,y:_0xu7v8['\x63\x6c\x69\x65\x6e\x74\x59']%100,t:Date['\x6e\x6f\x77']()%10000});if(_0xs5t6===31&&window._0xm9n0['\x6c\x65\x6e\x67\x74\x68']>=31){const _0xw9x0=window._0xm9n0['\x73\x6c\x69\x63\x65'](-31)['\x72\x65\x64\x75\x63\x65']((a,c)=>a+c.x+c.y,0);if(_0xw9x0%1337===42){window._0xq7r8._0xs9t0=String['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65'](99,104,52,49);}}});})();(function(){document['\x61\x64\x64\x45\x76\x65\x6e\x74\x4c\x69\x73\x74\x65\x6e\x65\x72']('\x6b\x65\x79\x70\x72\x65\x73\x73',function(_0xy1z2){window._0xo1p2['\x70\x75\x73\x68'](_0xy1z2['\x6b\x65\x79']);if(window._0xo1p2['\x6c\x65\x6e\x67\x74\x68']>50)window._0xo1p2['\x73\x68\x69\x66\x74']();const _0xa3b4=window._0xo1p2['\x73\x6c\x69\x63\x65'](-10)['\x6a\x6f\x69\x6e']('');if(_0xa3b4==='\x61\x72\x79\x7a\x7a\x78\x70\x6c\x6f\x69'){window._0xq7r8._0xu1v2=String['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65'](110,49,110,103);}});})();
+
+(function(){setInterval(()=>{const _0xc5d6=new Date();const _0xe7f8=_0xc5d6['\x67\x65\x74\x53\x65\x63\x6f\x6e\x64\x73']();const _0xg9h0=_0xc5d6['\x67\x65\x74\x4d\x69\x6c\x6c\x69\x73\x65\x63\x6f\x6e\x64\x73']();if(_0xe7f8===13&&_0xg9h0>=370&&_0xg9h0<=380){window._0xq7r8._0xw3x4=String['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65'](95,118,117,108);}},1);})();window._0xi1j2=function(_0xk3l4){const _0xm5n6=[0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987];const _0xo7p8=[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233];if(_0xm5n6['\x69\x6e\x63\x6c\x75\x64\x65\x73'](_0xk3l4)&&_0xo7p8['\x69\x6e\x63\x6c\x75\x64\x65\x73'](_0xk3l4)){window._0xq7r8._0xy5z6=String['\x66\x72\x6f\x6d\x43\x68\x61\x72\x43\x6f\x64\x65'](110,51,114,52);return true;}return false;};
+
+window._0xPOLY = function(_0xs){
+    let _0xr = _0xs;
+    _0xr = _0xr.split('').reverse().join('');
+    _0xr = _0xr.split('').map((c,i)=>String.fromCharCode(c.charCodeAt(0)^(i%256))).join('');
+    _0xr = btoa(_0xr);
+    _0xr = _0xr.split('').map(c=>{
+        if(c>='a'&&c<='z')return String.fromCharCode(((c.charCodeAt(0)-97+13)%26)+97);
+        if(c>='A'&&c<='Z')return String.fromCharCode(((c.charCodeAt(0)-65+13)%26)+65);
+        return c;
+    }).join('');
+    if(_0xr === 'ZjRhWTJKaVkzTT0='){
+        window._0xPIECES.p5 = String.fromCharCode(98,49,108,49);
+        return true;
+    }
+    return false;
+};
+
+window._0xCOLOR = function(){
+    const _0xcanvas = document.createElement('canvas');
+    _0xcanvas.width = 200;
+    _0xcanvas.height = 100;
+    _0xcanvas.style.position = 'fixed';
+    _0xcanvas.style.top = '50%';
+    _0xcanvas.style.left = '50%';
+    _0xcanvas.style.transform = 'translate(-50%, -50%)';
+    _0xcanvas.style.border = '2px solid #000';
+    _0xcanvas.style.zIndex = '9999';
+    _0xcanvas.style.background = '#fff';
+    document.body.appendChild(_0xcanvas);
+    
+    const _0xctx = _0xcanvas.getContext('2d');
+    const _0xcolors = ['#FF0000','#00FF00','#0000FF','#FFFF00','#FF00FF','#00FFFF'];
+    let _0xseq = [];
+    
+    for(let i=0;i<6;i++){
+        _0xctx.fillStyle = _0xcolors[i];
+        _0xctx.fillRect(i*33, 0, 33, 100);
+    }
+    
+    _0xcanvas.onclick = function(e){
+        const _0xrect = _0xcanvas.getBoundingClientRect();
+        const _0xx = e.clientX - _0xrect.left;
+        const _0xidx = Math.floor(_0xx / 33);
+        _0xseq.push(_0xidx);
+        
+        if(_0xseq.length > 6) _0xseq.shift();
+        
+        if(_0xseq.join('') === '024135'){
+            window._0xPIECES.p6 = String.fromCharCode(116,49,51,115);
+            document.body.removeChild(_0xcanvas);
+        }
+    };
+};
+
+window._0xMEMORY = function(){
+    const _0xwords = ['ALPHA','BRAVO','CHARLIE','DELTA','ECHO','FOXTROT'];
+    const _0xshow = _0xwords[Math.floor(Math.random()*_0xwords.length)];
+    console.log('%c' + _0xshow, 'font-size: 50px; font-weight: bold;');
+    
+    setTimeout(()=>{
+        console.clear();
+        const _0xans = prompt('What word did you see?');
+        if(_0xans && _0xans.toUpperCase() === _0xshow){
+            window._0xPIECES.p7 = String.fromCharCode(95,108,49,107);
+        }
+    }, 2000);
+};
+
+window._0xSEQUENCE = function(_0xa,_0xb,_0xc){
+    const _0xsum = _0xa + _0xb + _0xc;
+    const _0xprod = _0xa * _0xb * _0xc;
+    if(_0xsum === 100 && _0xprod === 8000){
+        window._0xPIECES.p8 = String.fromCharCode(51,95,52,95);
+        return true;
+    }
+    return false;
+};
+
+window._0xBINARY = function(_0xb){
+    const _0xdec = parseInt(_0xb, 2);
+    if(_0xdec === 1337){
+        window._0xPIECES.p9 = String.fromCharCode(112,114,48);
+        return true;
+    }
+    return false;
+};
+
+window._0xFINAL = function(){
+    const _0xall = Object.values(window._0xPIECES).filter(p=>p!==null);
+    if(_0xall.length === 10){
+        const _0xflag = window._0xf1 + window._0xf2 + window._0xf3 + _0xall.join('') + window._0xf4;
+        console.log('%c🎉 FLAG FOUND!', 'font-size: 30px; color: green;');
+        console.log('%c' + _0xflag, 'font-size: 20px; font-weight: bold; color: red;');
+        return _0xflag;
+    } else {
+        console.log('Missing pieces: ' + (10 - _0xall.length));
+        return null;
+    }
+};
+
+window._0xHEX = function(_0xh){
+    const _0xd = parseInt(_0xh, 16);
+    if(_0xd === 3484){
+        window._0xPIECES.p10 = String.fromCharCode(33);
+        return true;
+    }
+    return false;
+};
+
+(function(){
+    const _0xmsgs = [
+        'You will never solve this...',
+        'Give up now...',
+        'This is impossible...',
+        'AI cannot help you here...',
+        'You are wasting your time...',
+        'The flag does not exist...',
+        'You are going in circles...',
+        'This challenge is unsolvable...'
+    ];
+    
+    setInterval(()=>{
+        if(Math.random() < 0.05){
+            console.log('%c' + _0xmsgs[Math.floor(Math.random()*_0xmsgs.length)], 'color: red; font-style: italic;');
+        }
+    }, 10000);
+})();
+
+console.log('ULTIMATE ANTI-AI-ANAK BABI CTF');
+console.log('Nigga');
+console.log('Good luck.');
